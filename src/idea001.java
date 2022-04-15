849909a8ce01438e6c9f460f24d1399f34d6ef0e
lcd <1810750396@qq.com>
2022-04-15 17:27:28 +0800

    first commit

---
+++ src/idea001.java
@@ -0,0 +1,15 @@
+/*
+ * 太原工业学院计算机工程系
+ */
+
+/**
+ * @author lcd
+ * @version 1.0
+ * &date2022/4/15
+ */
+public class idea001 {
+    public static void main(String[] args) {
+        System.out.printf( "hello git");
+    }
+
+}
